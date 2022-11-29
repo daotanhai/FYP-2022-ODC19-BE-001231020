3f720b0ed3f4ae330e32d718af3c31c6be76d19b
Dao Tan Hai <63653854+daotanhai@users.noreply.github.com>
2022-11-30 02:33:18 +0700

    Change key with x at the latest
    remove x before use | This avoid leaking key warning from Amazon

--- AmazonCredentials/src/main/java/com/odc19/AmazonCredentials/AmazonKeys.java
+++ AmazonCredentials/src/main/java/com/odc19/AmazonCredentials/AmazonKeys.java
@@ -2,7 +2,7 @@ package com.odc19.AmazonCredentials;
 
 public final class AmazonKeys {
     public static final String END_POINT_URL = "https://s3.ap-southeast-1.amazonaws.com";
-    public static final String ACCESS_KEY = "AKIA6KG7TGAPAYLWPT7M";
-    public static final String SECRET_KEY = "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";
+    public static final String ACCESS_KEY = "AKIA6KG7TGAPAYLWPT7Mx";
+    public static final String SECRET_KEY = "UoDt1WHkWJtgOjwuvKKjn8wmDvEa7C5+78m7WMBkx";
     public static final String BUCKET_NAME = "odc19";
 }
